52623f8ebfbbb5bd00f1e6b350538727c1300589
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2023-10-15 15:35:31 -0700

    Myprintf

---
+++ print.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * _printf - A function that produces output according to a format.
+ * @format: is a character string.
+ * Return: Returns a value.
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list p;
+	int char_count = 0;
+
+	va_start(p, format);
+	while (*format != '\0')
+	{
+		if (*format != '%')
+		{
+			putchar(*format);
+			char_count++;
+		}
+		else
+		{
+			switch (*format++)
+			{
+			case 'c':
+			char_count += vfprintf(stdout, "%c", p);
+			break;
+			case 's':
+			char_count += vfprintf(stdout, "%s", p);
+			break;
+			case '%':
+			char_count += vfprintf(stdout, "%%", p);
+			break;
+			default:
+			putchar('%');
+			putchar(*format);
+			char_count += 2;
+			break;
+			}
+		}
+		format++;
+	}
+	va_end(p);
+	return (char_count);
+}
